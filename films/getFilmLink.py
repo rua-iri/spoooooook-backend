5c1b340011038a8b637c1dbda94d9fcb06b9e5cf
rua-iri <rory.mcguigan@protonmail.com>
2024-04-26 15:36:42 +0300

    update: remove jwt temporarily

--- films/getFilmLink.py
+++ films/getFilmLink.py
@@ -3,14 +3,14 @@ import helpers
 
 def lambda_handler(event, context):
 
-    jwt_token = event['headers'].get('authorization')
+    # jwt_token = event['headers'].get('authorization')
 
-    if not helpers.validateJWT(jwt_token):
-        return {
-            'statusCode': 403,
-            "headers": {"Access-Control-Allow-Origin": "*"},
-            'body': json.dumps({'error': 'Invalid Authorization Token'})
-        }
+    # if not helpers.validateJWT(jwt_token):
+    #     return {
+    #         'statusCode': 403,
+    #         "headers": {"Access-Control-Allow-Origin": "*"},
+    #         'body': json.dumps({'error': 'Invalid Authorization Token'})
+    #     }
 
 
     filmId = event["queryStringParameters"]["imdbID"]
