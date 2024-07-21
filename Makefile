90382d6b2f4796ad02bedf28874c4cf095c1c67b
rua-iri <rory.mcguigan@protonmail.com>
2024-07-21 08:57:59 +0300

    update: improve getAllFilms function

--- Makefile
+++ Makefile
@@ -1,9 +1,14 @@
 
 
+dev:
+	sam build && \
+	sam local start-api -l logfile.log
+
 
 all:
 	sam validate && \
 	sam build && \
 	sam package --region us-east-1 && \
 	sam deploy --region us-east-1
+
 
--- films/getAllFilms.py
+++ films/getAllFilms.py
@@ -1,5 +1,42 @@
 import json
 import helpers
+import logging
+
+logger = logging.getLogger(__name__)
+logging.basicConfig(level=logging.INFO)
+
+
+def processQuery(filmCategory):
+    try:
+        results_list: list = []
+
+        select_query: str = """SELECT film.imdb_id,
+                        poster_path
+                        FROM category
+                        INNER JOIN film_category
+                        ON category.category_id = film_category.category_id
+                        INNER JOIN film ON film_category.film_id = film.film_id
+                        WHERE category.category_title=(?);"""
+
+        all_results: list = helpers.selectQueryDB(
+            select_query,
+            (filmCategory, ),
+            True
+        )
+
+        # append each result to the results list
+        for singleResult in all_results:
+            filmMap = {
+                "imdbID": singleResult[0],
+                "posterPath": singleResult[1]
+            }
+
+            results_list.append(filmMap)
+
+        return results_list
+    except Exception as e:
+        raise e
+
 
 def lambda_handler(event, context):
 
@@ -12,38 +49,50 @@ def lambda_handler(event, context):
     #         'body': json.dumps({'error': 'Invalid Authorization Token'})
     #     }
 
-
-    filmGenre = event["queryStringParameters"]["category"]
-    filmResults = processQuery(filmGenre)
-
-    return {
-        'statusCode': 200,
+    response_body: dict = {
+        'statusCode': 500,
         "headers": {"Access-Control-Allow-Origin": "*"},
-        'body': json.dumps(filmResults)
+        'body': json.dumps({"status": "error", "error": "internal server error"})
     }
 
+    try:
 
+        print(event)
 
-def processQuery(filmCategory):
-    resultsList = []
+        film_genre: str = event["queryStringParameters"]["category"]
+        logger.info(f"Film Genre: {film_genre}")
 
-    selectQuery = """SELECT film.imdb_id, 
-                    poster_path 
-                    FROM category 
-                    INNER JOIN film_category 
-                    ON category.category_id = film_category.category_id 
-                    INNER JOIN film ON film_category.film_id = film.film_id 
-                    WHERE category.category_title=(?);"""
-    
-    allResults = helpers.selectQueryDB(selectQuery, (filmCategory, ), True)
-    
+        # ensure that film genre is provided
+        if not film_genre:
+            response_body.update({"statusCode": "400"})
+            response_body.update(
+                {'body': json.dumps(
+                    {"status": "error", "error": "Category is required"})
+                 }
+            )
+            return response_body
 
+        # query for films in this genre
+        film_results: list = processQuery(film_genre)
 
+        if not film_results:
+            response_body.update({"statusCode": "404"})
+            response_body.update(
+                {'body': json.dumps(
+                    {"status": "error", "error": "No results found"}
+                )}
+            )
+            return response_body
 
-    # append each result to the results list
-    for singleResult in allResults:
-        filmMap = {"imdbID": singleResult[0], "posterPath": singleResult[1]}
-        resultsList.append(filmMap)
+        response_body.update({'statusCode': 200})
+        response_body.update(
+            {'body': json.dumps({
+                "status": "success",
+                "data": film_results
+            })})
 
-    return resultsList
+        return response_body
 
+    except Exception as e:
+        logger.error(e)
+        return response_body
